6a1b0fc2900aec51c2477c4486900726d9161947
tanujgrover9 <tanujgrover07@gmail.com>
2025-10-29 23:54:44 +0530

    Your commit message

--- src/constants/index.js
+++ src/constants/index.js
@@ -164,11 +164,11 @@ export const projects = [
 ];
 
 export const socials = [
-  { name: "Instagram", href: "https://www.instagram.com/ali.sanatidev/reels/" },
-  {
-    name: "Youtube",
-    href: "https://www.youtube.com/channel/UCZhtUWTtk3bGJiMPN9T4HWA",
-  },
-  { name: "LinkedIn", href: "https://www.linkedin.com/in/ali-sanati/" },
-  { name: "GitHub", href: "https://github.com/Ali-Sanati" },
+  // { name: "Instagram", href: "" },
+  // {
+  //   name: "Youtube",
+  //   href: "https://www.youtube.com/channel/UCZhtUWTtk3bGJiMPN9T4HWA",
+  // },
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/anshuman-singh7/" },
+  { name: "GitHub", href: "https://github.com/tanujgrover9" },
 ];
